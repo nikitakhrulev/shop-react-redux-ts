deca47b1cadfbc5243eeef9a8474ad7dbf0754ee
Nikita <dubblerner.2@gmail.com>
2024-09-18 01:30:12 +0300

    first commit

---
+++ src/components/layout/Header.tsx
@@ -0,0 +1,34 @@
+import logo from '../../img/logo.svg';
+import user from '../../img/user.svg';
+import favorite from '../../img/favorite.svg';
+import cart from '../../img/cart.svg';
+import search from '../../img/search.svg';
+import { ArrayTypeNode, TupleType } from 'typescript';
+
+function Header() {
+    return <header className="header">
+        <div className="container">
+            <div className="header__row">
+                <img src={logo} alt="logo" />
+                <div className="user-field">
+                    <div className="user-field">
+                        <img src={user} alt="user" />
+                        <span className="username">User</span>
+                    </div>
+                </div>
+                <div className="header__search">
+                    <form className="form">
+                        <img src={search} alt="search" />
+                        <input type="text" placeholder='Search for anything...'/>
+                    </form>
+                </div>
+                <div className="header__actions">
+                    <img src={favorite} alt="favorite" />
+                    <img src={cart} alt="cart" />
+                </div>
+            </div>
+        </div>
+    </header>
+}
+
+export { Header }
